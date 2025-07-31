35466917229da6a0b17bfdcde633630b129a0bd4
CarduhGangGang <dudajunkysk8@gmail.com>
2025-07-31 17:45:02 +0100

    Revert "Update registerContentRoutes.js"
    This reverts commit 2604f4d2b232dde0d4ea075a7bde9ce90c625b23.

--- Backend/routes/registerContentRoutes.js
+++ Backend/routes/registerContentRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/registerContentController');
 
-// ✅ A rota externa já é "/api/register-content", então aqui é só "/"
-router.get('/', controller.getRegisterContent);
-router.put('/', controller.updateRegisterContent);
+router.get('/register-content', controller.getRegisterContent);
+router.put('/register-content', controller.updateRegisterContent);
 
 module.exports = router;
+
+
