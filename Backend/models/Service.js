c56984a98d22f1f4f46694da2ab22cc8fb9a3904
CarduhGangGang <dudajunkysk8@gmail.com>
2025-08-05 12:16:35 +0100

    FSSF

--- Backend/models/Service.js
+++ Backend/models/Service.js
@@ -1,12 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
   const Servico = sequelize.define("Servico", {
     nome: DataTypes.STRING,
+    descricao: DataTypes.TEXT,
     duracao: DataTypes.INTEGER,
-    preco: DataTypes.DECIMAL
+    preco: DataTypes.DECIMAL,
+    imageUrl: DataTypes.STRING,
+  }, {
+    tableName: "Servicos", // Nome fixo da tabela
+    timestamps: true,      // Ativa createdAt/updatedAt
   });
 
   Servico.associate = (models) => {
-    Servico.hasMany(models.Agendamento, { foreignKey: "servico_id" });
+    Servico.hasMany(models.Agendamento, {
+      foreignKey: "servico_id",
+      as: "Agendamentos",
+    });
   };
 
   return Servico;
